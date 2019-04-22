d5960213241e542d634aa6631c3851a530e20581
Leonardo <leonardo.lourenco@ftdata.com.br>
2019-04-22 10:22:56 -0300

    versão 1.0.0

--- app/src/main/java/com/example/simpleReader/MainActivity.java
+++ app/src/main/java/com/example/simpleReader/MainActivity.java
@@ -1,13 +1,10 @@
-package com.example.jailtoncode;
+package com.example.simpleReader;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -15,43 +12,32 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnScan;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnScan = findViewById(R.id.btnScan);
-        final Activity activity = this;
-
-        btnScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Camera Scan");
-                integrator.setCameraId(0);
-                integrator.initiateScan();
-            }
-        });
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        intentIntegrator.setPrompt("Aponte para o QRCode!");
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.initiateScan();
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
         if (result != null) {
-            if(result.getContents()!= null){
-                OpenChrome(result);
-            }else{
-                alert("Scan c" + "ancelado!");
+            if (result.getContents() != null) {
+                launchBrowser(result);
+            } else {
+                Toast.makeText(getApplicationContext(), "Scan cancelado!", Toast.LENGTH_LONG).show();
+                finish();
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    private void OpenChrome(IntentResult result) {
+    private void launchBrowser(IntentResult result) {
         String url = result.getContents();
         Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -64,8 +50,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void alert(String msg){
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-
-    }
 }
